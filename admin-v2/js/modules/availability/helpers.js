5f6c41b9affa263022b675129e1eca78631eb817
ceybreez <sandzz.ash@gmail.com>
2026-07-06 17:55:01 +0400

    Update helpers.js

--- admin-v2/js/modules/availability/helpers.js
+++ admin-v2/js/modules/availability/helpers.js
@@ -111,6 +111,4 @@ export function escapeHtml(value) {
     .replaceAll('"', "&quot;")
     .replaceAll("'", "&#039;");
 }
-export function dateOnly(value) {
-  return String(value || "").slice(0, 10);
-}
+
